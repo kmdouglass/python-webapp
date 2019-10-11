b9df2ba98c05abaadd0a661d77aeedeec8c4ad37
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2019-10-11 14:50:03 +0200

    Added a postgres database service to the stack

--- project/project/settings.py
+++ project/project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database1',
+        'USER': 'database1_role',
+        'PASSWORD': 'database1_password',
+        'HOST': 'database1',
+        'PORT': '5432',
     }
 }
 
